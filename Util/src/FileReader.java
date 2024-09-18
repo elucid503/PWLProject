983fbe490534d1c0541e95a3c5775ae8bbdd94b3
elucid503 <elucid@duck.com>
2024-09-18 08:36:17 -0400

    Changes to file reader class

--- Util/src/FileReader.java
+++ Util/src/FileReader.java
@@ -1,8 +1,11 @@
+import java.io.BufferedReader;
 import java.io.File;
 
 public class FileReader {
 
-    public FileReader(String Path) {
+    private final String Path;
+
+    public FileReader(String Path) throws IllegalArgumentException {
 
         // Resolve the path and throw an error if not found
 
@@ -13,6 +16,38 @@ public class FileReader {
             throw new IllegalArgumentException("File not found");
 
         }
+
+        this.Path = Path;
+
+    }
+
+    public String ReadAsString() throws Exception {
+
+        // Read the file as a string
+
+        BufferedReader Reader = new BufferedReader(new java.io.FileReader(Path));
+
+        StringBuilder FileContents = new StringBuilder();
+        String Line;
+
+        try {
+
+            while ((Line = Reader.readLine()) != null) {
+
+                FileContents.append(Line);
+                FileContents.append("\n");
+
+            }
+
+        } catch (Exception e) {
+
+            // Read exception
+
+            throw new Exception("Error reading file");
+
+        }
+
+        return FileContents.toString();
 
     }
 
