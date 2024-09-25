a52a71b3055e2489812873c262178496934122d9
elucid503 <elucid@duck.com>
2024-09-25 08:29:47 -0400

    Add Article and Topic class

---
+++ Project/src/Util/Structs/Article.java
@@ -0,0 +1,13 @@
+package Util.Structs;
+
+public class Article {
+
+    private final String filePath;
+
+    public Article(String filePath) {
+
+        this.filePath = filePath;
+
+    }
+
+}
---
+++ Project/src/Util/Structs/Topic.java
@@ -0,0 +1,41 @@
+package Util.Structs;
+
+import Util.Files.DirReader;
+
+import java.util.ArrayList;
+
+public class Topic {
+
+    public String topicName;
+
+    public ArrayList<Article> articleList;
+
+    private final String dirPath;
+
+    public Topic(String topicName) {
+
+        this.topicName = topicName;
+
+        this.dirPath = "./ExternalFiles/" + topicName;
+
+    }
+
+    public void Load() throws Exception {
+
+        DirReader dirReader = new DirReader(dirPath);
+
+        ArrayList<String> articleFileList = dirReader.Read();
+
+        for (String articleFile : articleFileList) {
+
+            Article article = new Article(articleFile);
+
+            // article.Load(); implemented soon
+
+            articleList.add(article);
+
+        }
+
+    }
+
+}
