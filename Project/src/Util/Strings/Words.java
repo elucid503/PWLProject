6ac0cc8ea6ffa6347ffc910242b94049ab549b79
jamesdoyle <james.doyle@student.fairfield.edu>
2024-10-02 08:58:51 -0400

    I did work but in reality it was paul
    do you want a cookie

--- Project/src/Util/Strings/Words.java
+++ Project/src/Util/Strings/Words.java
@@ -1,5 +1,22 @@
 package Util.Strings;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Words {
+
+    public ArrayList<String> convertSentencesToWords(ArrayList<String> sentences) {
+
+        ArrayList<String> result = new ArrayList<>();
+
+        for (String sentence : sentences) {
+
+            result.addAll(Arrays.asList(sentence.split(" ")));
+
+        }
+
+        return result;
+
+    }
 
 }
