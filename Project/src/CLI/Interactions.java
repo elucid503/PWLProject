c0c34526f786fedc217021756f55540531c67fe9
elucid503 <elucid@duck.com>
2024-11-13 09:13:13 -0500

    Small enhancements to work done on 11/13

--- Project/src/CLI/Interactions.java
+++ Project/src/CLI/Interactions.java
@@ -29,14 +29,17 @@ public class Interactions {
 
 
         Logging.logUI("Welcome to the Sentiment Analysis Tool", new String[]{Logging.BOLD,Logging.CYAN});
+
+        Logging.lineBreak();
         Logging.horizontalLine();
-        Logging.logUI("1. Create a Topic > Prompts user for topic name, creates directory", new String[]{Logging.ITALIC});
+
+        Logging.logUI("1. Create a Topic", new String[]{Logging.ITALIC});
         Logging.smartHorizontalLine();
-        Logging.logUI("2. Select a Topic > Gives a list of topics, select one to run analysis", new String[]{Logging.ITALIC});
+        Logging.logUI("2. Select a Topic", new String[]{Logging.ITALIC});
         Logging.smartHorizontalLine();
-        Logging.logUI("3. Add an Article > Prompts user for article URL (or file name), downloads article", new String[]{Logging.ITALIC});
+        Logging.logUI("3. Add an Article", new String[]{Logging.ITALIC});
         Logging.smartHorizontalLine();
-        Logging.logUI("4. Remove an Article > Select topic, then prompts user to remove an article", new String[]{Logging.ITALIC});
+        Logging.logUI("4. Remove an Article", new String[]{Logging.ITALIC});
         Logging.smartHorizontalLine();
         Logging.logUI("5. Exit", new String[]{Logging.BOLD,Logging.RED});
 
